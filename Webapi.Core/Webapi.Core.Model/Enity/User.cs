0cf45b925e7034feb948139fa15b83816cdb1869
huguodong <531035580@qq.com>
2020-05-18 13:44:01 +0800

    new
    new

---
+++ Webapi.Core/Webapi.Core.Model/Enity/User.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using SqlSugar;
+
+namespace Webapi.Core.Model.Enity
+{
+    ///<summary>
+    ///用户表
+    ///</summary>
+    [SugarTable("User")]
+    public partial class User
+    {
+        public User()
+        {
+
+
+        }
+        /// <summary>
+        /// Desc:用户ID
+        /// Default:
+        /// Nullable:False
+        /// </summary>           
+        [SugarColumn(IsPrimaryKey = true, IsIdentity = true)]
+        public int UserId { get; set; }
+
+        /// <summary>
+        /// Desc:用户名
+        /// Default:
+        /// Nullable:True
+        /// </summary>           
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// Desc:年龄
+        /// Default:
+        /// Nullable:True
+        /// </summary>           
+        public int? Age { get; set; }
+
+    }
+}
