488cd9c225e19828fff48ddc93d6db3b38dd9160
huguodong <531035580@qq.com>
2020-05-21 13:41:43 +0800

    helper
    helper

---
+++ Webapi.Core/Webapi.Core.Common/Helper/DateHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Webapi.Core.Common.Helper
+{
+    public class DateHelper
+    {
+        /// <summary>
+        /// Stamp转datetime
+        /// </summary>
+        /// <param name="time"></param>
+        /// <returns></returns>
+        public static DateTime StampToDateTime(string time)
+        {
+            time = time.Substring(0, 10);
+            double timestamp = Convert.ToInt64(time);
+            System.DateTime dateTime = new System.DateTime(1970, 1, 1, 0, 0, 0, 0);
+            dateTime = dateTime.AddSeconds(timestamp).ToLocalTime();
+            return dateTime;
+        }
+    }
+}
