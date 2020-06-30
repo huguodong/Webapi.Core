d1eb0c035929a5ebad1c0fceeb52a219557f651c
Tiger <531035580@QQ.COM>
2020-06-30 10:30:46 +0800

    add
    add

--- Webapi.Core/Webapi.Core.IRepository/Base/IBaseRepository.cs
+++ Webapi.Core/Webapi.Core.IRepository/Base/IBaseRepository.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 
 namespace Webapi.Core.IRepository.Base
@@ -12,11 +12,43 @@ namespace Webapi.Core.IRepository.Base
     public interface IBaseRepository<TEntity> where TEntity : class
     {
         /// <summary>
+        /// 条件查询列表
+        /// </summary>
+        /// <param name="whereExpression"></param>
+        /// <returns></returns>
+        Task<List<TEntity>> QueryList(Expression<Func<TEntity, bool>> whereExpression);
+
+
+
+        /// <summary>
+        /// 条件查询一条返回实体
+        /// </summary>
+        /// <param name="whereExpression"></param>
+        /// <returns></returns>
+        Task<TEntity> Query(Expression<Func<TEntity, bool>> whereExpression);
+
+        /// <summary>
+        /// 分页查询
+        /// </summary>
+        /// <param name="pageIndex"></param>
+        /// <param name="pageSize"></param>
+        /// <param name="totalNumber"></param>
+        /// <param name="totalPage"></param>
+        /// <returns></returns>
+        Task<Tuple<List<TEntity>, int, int>> PageList(int pageIndex, int pageSize);
+
+        /// <summary>
         /// 根据ID查询
         /// </summary>
         /// <param name="objId"></param>
         /// <returns></returns>
-        Task<TEntity> QueryByID(object objId);
+        Task<TEntity> GetById(object objId);
+
+        /// <summary>
+        /// 查询所有
+        /// </summary>
+        /// <returns></returns>
+        Task<List<TEntity>> GetList();
 
         /// <summary>
         /// 添加
@@ -33,11 +65,18 @@ namespace Webapi.Core.IRepository.Base
         Task<bool> Update(TEntity model);
 
         /// <summary>
-        /// 删除
+        /// 删除一条信息
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task<bool> DeleteById(dynamic id);
+
+        /// <summary>
+        /// 根据主键数组删除
         /// </summary>
         /// <param name="ids"></param>
         /// <returns></returns>
-        Task<bool> DeleteByIds(object[] ids);
+        Task<int> DeleteByIds(object[] ids);
 
     }
 }
--- Webapi.Core/Webapi.Core.Repository/Base/BaseRepository.cs
+++ Webapi.Core/Webapi.Core.Repository/Base/BaseRepository.cs
@@ -1,7 +1,7 @@
-﻿using System;
+﻿using SqlSugar;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 using Webapi.Core.IRepository.Base;
 
@@ -10,7 +10,6 @@ namespace Webapi.Core.Repository.Base
     public class BaseRepository<TEntity> : DbContext<TEntity>, IBaseRepository<TEntity> where TEntity : class, new()
     {
 
-
         /// <summary>
         /// 写入实体数据
         /// </summary>
@@ -18,20 +17,35 @@ namespace Webapi.Core.Repository.Base
         /// <returns></returns>
         public async Task<bool> Add(TEntity model)
         {
-            var i = await Task.Run(() => CurrentDb.Insert(model));
-            //返回的i是long类型,这里你可以根据你的业务需要进行处理
-            return i;
+            return await Task.Run(() =>
+            {
+                var result = CurrentDb.AsInsertable(model).IgnoreColumns(true).ExecuteCommand();
+                return result > 0;
+            });
+
+
         }
 
         /// <summary>
-        /// 根据ID删除
+        /// 根据主键删除
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task<bool> DeleteById(dynamic id)
+        {
+            return await Task.Run(() => CurrentDb.DeleteById(id));
+        }
+
+
+        /// <summary>
+        /// 根据主键数组删除
         /// </summary>
         /// <param name="ids"></param>
         /// <returns></returns>
-        public async Task<bool> DeleteByIds(object[] ids)
+        public async Task<int> DeleteByIds(object[] ids)
         {
-            var i = await Task.Run(() => Db.Deleteable<TEntity>().In(ids).ExecuteCommand());
-            return i > 0;
+
+            return await Task.Run(() => Db.Deleteable<TEntity>().In(ids).With(SqlWith.RowLock).ExecuteCommand());
         }
 
         /// <summary>
@@ -39,9 +53,50 @@ namespace Webapi.Core.Repository.Base
         /// </summary>
         /// <param name="objId"></param>
         /// <returns></returns>
-        public async Task<TEntity> QueryByID(object objId)
+        public async Task<TEntity> GetById(object objId)
         {
             return await Task.Run(() => Db.Queryable<TEntity>().InSingle(objId));
+        }
+
+        /// <summary>
+        /// 获取所有
+        /// </summary>
+        /// <returns></returns>
+        public async Task<List<TEntity>> GetList()
+        {
+            return await Task.Run(() => Db.Queryable<TEntity>().ToList());
+        }
+        /// <summary>
+        /// 分页
+        /// </summary>
+        /// <param name="pageIndex"></param>
+        /// <param name="pageSize"></param>
+        /// <returns></returns>
+        public async Task<Tuple<List<TEntity>, int, int>> PageList(int pageIndex, int pageSize)
+        {
+            return await Task.Run(() =>
+        {
+            int totalNumber = 0;
+            int totalPage = 0;
+            var data = CurrentDb.AsQueryable().ToPageList(pageIndex, pageSize, ref totalNumber, ref totalPage);
+            return new Tuple<List<TEntity>, int, int>(data, totalNumber, totalPage);
+        });
+
+        }
+
+        public async Task<TEntity> Query(Expression<Func<TEntity, bool>> whereExpression)
+        {
+            return await Db.Queryable<TEntity>().WhereIF(whereExpression != null, whereExpression).FirstAsync();
+        }
+
+        /// <summary>
+        /// 条件查询
+        /// </summary>
+        /// <param name="whereExpression"></param>
+        /// <returns></returns>
+        public async Task<List<TEntity>> QueryList(Expression<Func<TEntity, bool>> whereExpression)
+        {
+            return await Db.Queryable<TEntity>().WhereIF(whereExpression != null, whereExpression).ToListAsync();
         }
 
         /// <summary>
@@ -52,7 +107,7 @@ namespace Webapi.Core.Repository.Base
         public async Task<bool> Update(TEntity model)
         {
             //这种方式会以主键为条件
-            var i = await Task.Run(() => Db.Updateable(model).ExecuteCommand());
+            var i = await Task.Run(() => Db.Updateable(model).IgnoreColumns(ignoreAllNullColumns: true).ExecuteCommand());
             return i > 0;
         }
     }
